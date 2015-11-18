a694d585ae4d18de5602922696658d1d23f95e32
Bryan Chau <bchau93@hotmail.com>
2015-11-18 15:05:45 -0800

    Added Options model

---
+++ src/TeamMVC6/Models/Options.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamMVC6.Models
+{
+    public class Options
+    {
+        public int OptionsId { get; set; }
+
+        [Required]
+        [MaxLength(50), MinLength(3)]
+        public string Title { get; set; }
+
+        public bool isActive { get; set; }
+    }
+}
