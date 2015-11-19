2e4cd42a2897dc6cedd68092d21d6a7c71152742
TheLawrence <lawsarmiento@gmail.com>
2015-11-18 16:21:19 -0800

    Added YearTerm model

---
+++ src/TeamMVC6/Models/YearTerm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamMVC6.Models
+{
+    public class YearTerm
+    {
+        public int YearTermId { get; set; }
+        public int Year { get; set; }
+        [RegularExpression(@"(30)|(20)|(10)$", ErrorMessage = "Please enter a valid Term")]
+        public int Term { get; set; }
+        public Boolean IsDefault { get; set; }
+    }
+}
