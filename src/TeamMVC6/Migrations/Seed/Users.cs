4a93798e1d85b982e7e9a3c009ddabff7e6508d0
Bryan Chau <bchau93@hotmail.com>
2015-11-24 11:50:17 -0800

    Added options to navbar

---
+++ src/TeamMVC6/Migrations/Seed/Users.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamMVC6.Models;
+
+namespace TeamMVC6.Migrations.Seed
+{
+    public static class Users
+    {
+        public static void Initialize(OptionsContext context)
+        {
+            ApplicationDbContext.            
+        }
+    }
+}
