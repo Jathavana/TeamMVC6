5679a512489c35c2ee5fe701dcb9ca658656aab6
TheLawrence <lawsarmiento@gmail.com>
2015-11-27 11:59:55 -0800

    commit to repo before pulling users stuff

--- src/TeamMVC6/Controllers/RolesController.cs
+++ src/TeamMVC6/Controllers/RolesController.cs
@@ -56,36 +56,33 @@ namespace TeamMVC6.Controllers
 
         public async Task<IActionResult> Edit(string id)
         {
-            //IdentityRole role = _context.Roles
-            //    .Where(c => c.Id == id)
-            //    .FirstOrDefault();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
 
-            //if (id == null)
-            //{
-            //    return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest);
-            //}
-
-            //IdentityRole role = _context.Roles.Where(c => c.Id == id).FirstOrDefault();
-            IdentityRole role = await _roleManager.FindByIdAsync(id);
-
-            ViewBag.test = role.Name;
-            //if (role == null)
-            //{
-            //    return HttpNotFound();
-            //}
+            IdentityRole role = _context.Roles.Where(c => c.Id == id).FirstOrDefault();
+            //IdentityRole role = await _roleManager.FindByIdAsync(id);
+            
+            //ViewBag.test = _context.Users;
+            var test = role.Users;
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
 
             //New attempt
-            //var userDictionary = new Dictionary<string, string>();
-            //if (_context.Users.Count() > 0)
-            //{
-            //    foreach (var user in _context.Users)
-            //    {
-            //        userDictionary[user.Id] = user.UserName;
-            //    }
-            //}
-            //ViewBag.UserDictionary = userDictionary;
+            var userDictionary = new Dictionary<string, string>();
+            if (_context.Users.Count() > 0)
+            {
+                foreach (var user in _context.Users)
+                {
+                    userDictionary[user.Id] = user.UserName;
+                }
+            }
+            ViewBag.UserDictionary = userDictionary;
 
-            return View();
+            return View(role);
         }
 
         public IActionResult Delete(string id)
@@ -115,5 +112,84 @@ namespace TeamMVC6.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        //public async Task<ActionResult> RemoveUserFromRole(string roleId, string userId)
+        //{
+        //    IdentityRole role = _context.Roles.Where(c => c.Id == roleId).FirstOrDefault();
+        //    IdentityUserRole identityUserRole = role.Users
+        //        .Where(c => c.RoleId == roleId)
+        //        .Where(c => c.UserId == userId)
+        //        .First();
+
+        //    _roleManager.FindByIdAsync(roleId).Users.Remove(identityUserRole);
+
+        //    var test = _roleManager.FindByIdAsync(roleId).Users;
+
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Edit/" + role.Id);
+        //}
+
+        //public ActionResult AddUserToRole(string roleId)
+        //{
+        //    IdentityRole role = db.Roles.Find(roleId);
+        //    List<SelectListItem> ddlDataSource = new List<SelectListItem>();
+        //    var result = db.Users.OrderBy(c => c.Id);
+        //    if (result.Count() > 0)
+        //    {
+        //        foreach (var user in result)
+        //        {
+        //            ddlDataSource.Add(new SelectListItem
+        //            {
+        //                Text = user.UserName,
+        //                Value = user.Id,
+        //            });
+        //        }
+        //    }
+        //    ViewBag.Users = ddlDataSource;
+
+        //    return View(role);
+        //}
+
+        //public ActionResult AddUserToRoleSubmit(string ddlUsers, string roleId)
+        //{
+        //    var role = db.Roles.Where(c => c.Id == roleId).First();
+
+        //    List<SelectListItem> ddlDataSource = new List<SelectListItem>();
+        //    var result = db.Users.OrderBy(c => c.Id);
+        //    if (result.Count() > 0)
+        //    {
+        //        foreach (var user in result)
+        //        {
+        //            ddlDataSource.Add(new SelectListItem
+        //            {
+        //                Text = user.UserName,
+        //                Value = user.Id,
+        //            });
+        //        }
+        //    }
+        //    ViewBag.Users = ddlDataSource;
+
+        //    //Validation
+        //    if (role.Users.Count > 0)
+        //    {
+        //        foreach (var user in role.Users)
+        //        {
+        //            if (user.UserId == ddlUsers)
+        //            {
+        //                ViewBag.InUse = "This user is already a member of the Role";
+        //                return View("AddUserToRole", role);
+        //            }
+        //        }
+        //    }
+
+        //    db.Roles.Where(c => c.Name == role.Name).First().Users.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityUserRole
+        //    {
+        //        RoleId = role.Id,
+        //        UserId = ddlUsers
+        //    });
+        //    db.SaveChanges();
+
+        //    return RedirectToAction("Edit/" + role.Id);
+        //}
     }
 }
